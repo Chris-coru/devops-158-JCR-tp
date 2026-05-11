17fd4dd29a4683f1eb48ef59328466e25bc2370d
Juan CottonRussell <Chris-coru@users.noreply.github.com>
2026-05-11 11:24:19 +0200

    Test webhook Jenkins

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "A Warrior acts as if he knows what he is doing, when in effect he knows nothing. -D.J."
+	return "A Warrior acts as if he knows what he is doing - version webhook OK."
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
